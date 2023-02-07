4bf5a1039d22e8da60cc5380bdaa26b936e76a91
LalehPouyan <124587215+LalehPouyan@users.noreply.github.com>
2023-02-07 11:00:29 +0100

    Update env.sh

--- arch/ecmwf/aurora/nec/4.0.0/env.sh
+++ arch/ecmwf/aurora/nec/4.0.0/env.sh
@@ -12,37 +12,18 @@ module_unload() {
   module unload $1
 }
 
-# Unload to be certain
-#module_unload boost
-#module_unload cmake
-#module_unload intel
-#module_unload pgi
-#module_unload gnu
 
-# Load modules
-#module use /opt/nvidia/hpc_sdk/modulefiles
-## module load nvhpc
-#module load nvhpc-nompi/20.9
-#module_load boost/1.61.0
-#module_load cmake/3.14.5
 export FC=nfort
 export CC=ncc
 export CXX=nc++
 
 set -x
 
-# Increase stack size to maximum
-ulimit -S -s unlimited
 
 # Enable floating point error trapping at run time
 export VE_FPE_ENABLE=DIV,INV,FOF,FUF,INE
 
 
-# Fix boost header location
-#export BOOST_INCLUDEDIR="/usr/local/apps/boost/1.61.0/PGI/17.1/include/"
-
-# Include local OpenMPI in the path for discovery in build
-export PATH="/local/hdd/nabr/openmpi/nvhpc-nompi/20.9/bin:$PATH"
 
 # Restore tracing to stored setting
 if [[ -n "$tracing_" ]]; then set -x; else set +x; fi
